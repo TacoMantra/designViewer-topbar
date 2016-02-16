2948c1c6eb9a9a6485b31279f871544d23f9fd09
Jon Miller <jon@cpasitesolutions.com>
2016-02-16 18:20:52 -0500

    Initial commit

---
+++ stylebar-2016.php
@@ -0,0 +1,11 @@
+
+<!-- Google Fonts -->
+<link href='http://fonts.googleapis.com/css?family=Roboto:300,100|Open+Sans:400,300italic,300,400italic,600,600italic' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" href="css/stylebar2016.css">
+
+<div class="stylebar-wrapper">
+	<div class="stylebar-container">
+		
+	</div>
+</div>
