3298b48d0720fd5007527595b749cb2239964e02
Jon Miller <jon@cpasitesolutions.com>
2016-04-20 16:28:25 -0400

    added support for adaptive wireframe images

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,8 +11,6 @@ $(document).ready(function(){
 	// Resize to selected device
 	function viewportResize(device) 
 	{
-		// Resize the iframe to the selected device dimensions
-		$('#stylebar-site-container').removeClass().addClass('stylebar-' + device);
 		// Make appropriate adjustements to body overflow
 		$('#stylebar-site-container').removeClass().addClass('stylebar-resp stylebar-' + device);
 	}
@@ -76,21 +74,64 @@ $(document).ready(function(){
 
 	$('#stylebar-selector').change(function()
 	{
-		var selected = $("option:selected", this).val();
+		var selected = $("option:selected", this),
+			layout = selected.val(),
+			layoutType = selected.parent('optgroup').attr('label');
 
-		switch (selected) {
-			case 'base1':
+		// Get rid of all the responsive stuff if adaptive layout chosen
+		if (layoutType == "Adaptive Base Layouts")
+		{
+			$('.stylebar-viewport-container').children('.stylebar-device-icon').not('.stylebar-emulate-desktop').css('visibility', 'hidden');
+
+			if(!$('#stylebar-site-container').hasClass('stylebar-desktop'))
+			{
+				// Resize the iframe to the device dimensions
+				viewportResize('desktop');
+
+				// Set the iframe to the height of the viewport
+				$('#stylebar-site').height(height);
+
+				// Assign the active state to the correct device icon
+				$('.stylebar-active-device').removeClass('stylebar-active-device');
+				$('.stylebar-emulate-desktop').addClass('stylebar-active-device');
+			}
+		}
+		else
+		{
+			$('.stylebar-viewport-container').children('.stylebar-device-icon').not('.stylebar-emulate-desktop').css('visibility', 'visible');
+		}
+
+		switch (layout) {
+			case 'base1R':
 				updateIframe('http://baselayouts53354.td.prosites.com/');
 				break;
-			case 'base2':
+			case 'base2R':
 				updateIframe('http://baselayouts70584.td.prosites.com/');
 				break; 
-			case 'base3':
+			case 'base3R':
 				updateIframe('http://baselayouts20395.td.prosites.com/');
 				break; 
-			case 'base4':
+			case 'base4R':
 				updateIframe('http://baselayouts37025.td.prosites.com/');
 				break; 
+			case 'base1A':
+				updateIframe('http://www.prosites.com/clients/CustomSite-BaseLayouts/BaseLayout1A.aspx');
+				break;
+			case 'base2A':
+				updateIframe('http://www.prosites.com/clients/CustomSite-BaseLayouts/BaseLayout2A.aspx');
+				break; 
+			case 'base3A':
+				updateIframe('http://www.prosites.com/clients/CustomSite-BaseLayouts/BaseLayout3A.aspx');
+				break; 
+			case 'base4A':
+				updateIframe('http://www.prosites.com/clients/CustomSite-BaseLayouts/BaseLayout4A.aspx');
+				break; 
+			case 'base5A':
+				updateIframe('http://www.prosites.com/clients/CustomSite-BaseLayouts/BaseLayout5A.aspx');
+				break;
+			case 'base6A':
+				updateIframe('http://www.prosites.com/clients/CustomSite-BaseLayouts/BaseLayout6A.aspx');
+				break;
 			default:
 				updateIframe('http://baselayouts53354.td.prosites.com/');
 		}
